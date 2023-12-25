52c80c63d76ddf5e9b0055402a39a64c5c5c3023
nextbirthday <dumarsyoon@gmail.com>
2023-12-25 16:10:04 +0900

    🍕 Youtube thumbnail css updated

--- pocky/src/app/(route)/youtube/components/videoItem.tsx
+++ pocky/src/app/(route)/youtube/components/videoItem.tsx
@@ -4,11 +4,12 @@ import styles from './videoItem.module.css'
 import Link from 'next/link'
 const VideoItem = (props: any) => {
   const { video, selectVideo } = props
+  console.log(video)
   return (
     <Link href={`/watch/${video.id.videoId}`} className={styles.thumbnail_box}>
       <div className={styles.video_item} /* onClick={() => selectVideo(video)} */>
         <div>
-          <img className={styles.video_thumbnail} src={video.snippet.thumbnails.medium.url} alt="썸네일" />
+          <img className={styles.video_thumbnail} src={video.snippet.thumbnails.high.url} alt="썸네일" />
         </div>
         <div>
           <p className={styles.video_title} key={video.id}>
